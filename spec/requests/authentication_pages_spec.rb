6063d5340da72e159ce55c341b252a19232834f8
Tyler J Thomas <tye.thomas17@gmail.com>
2014-05-08 16:24:45 -0600

    Finish user edit, update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,32 +20,32 @@ describe "Authentication" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-danger') }
 
-    describe "after visiting another page" do
+      describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-danger') }
       end
     end
 
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-    end
-
-      it { should have_title(user.name) }
-      it { should have_link('Users',       href: users_path) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-    
-      describe "followed by signout" do
-          before { click_link "Sign out" }
-          it { should have_link('Sign in') }
+      describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          fill_in "Email",    with: user.email.upcase
+          fill_in "Password", with: user.password
+          click_button "Sign in"
         end
-    end 
+
+        it { should have_title(user.name) }
+        it { should have_link('Users',       href: users_path) }
+        it { should have_link('Profile',     href: user_path(user)) }
+        it { should have_link('Settings',    href: edit_user_path(user)) }
+        it { should have_link('Sign out',    href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
+      
+        describe "followed by signout" do
+            before { click_link "Sign out" }
+            it { should have_link('Sign in') }
+        end
+      end 
   end
 
   describe "authorization" do
@@ -90,6 +90,7 @@ describe "Authentication" do
     end
 
     describe "as wrong user" do
+
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user, no_capybara: true }
@@ -105,6 +106,19 @@ describe "Authentication" do
         #specify { expect(response).to redirect_to(root_url) }
       end
     end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        #specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
   end
   
 end
