33fe713bbd9fcb38a0ebd4a8c6060e2ecea6fa5d
Tyler J Thomas <tye.thomas17@gmail.com>
2014-05-13 15:32:03 -0600

    Follow and Unfollow

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,7 +65,7 @@ describe User do
   it { should respond_to(:followers) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
-  it { should respond_to(:unfollow!) }
+  it { should respond_to(:unfollow!) }  
 
   it { should be_valid }
   it { should_not be_admin }
